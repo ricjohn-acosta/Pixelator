e820a0a67303af24227260a62156221d70309ce2
EDA <james.panaho@gmail.com>
2020-01-31 10:20:28 +1300

    right side almost done

---
+++ client/components/GalleryFiveArts.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link, Route } from 'react-router-dom'
+
+const GalleryFiveArts = (props) => {
+  return (
+    <div>
+    {console.log('Gal5Arts')}
+    <img src="" />
+    </div>
+
+  )
+}
+
+export default GalleryFiveArts
--- client/components/Right.jsx
+++ client/components/Right.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import { Link, Route } from 'react-router-dom'
+
+import GalleryFive from './GalleryFive'
+import GalleryEight from './GalleryEight'
+import GalleryFiveArts from './GalleryFiveArts'
 
 const Right = (props) => {
   return (
     <>
-    <h1>This will contain the RIGHT elements</h1>
+    <h1>Art Gallery</h1>
+    <div>
+      <Route path="/" component={ GalleryFive } />
+      <Route path="/gal5" component={ GalleryFiveArts } />
+      <Route path="/" component={ GalleryEight } />
+      {console.log('Main Right')}
+    </div>
     </>
   )
 }
 
 export default Right
-
