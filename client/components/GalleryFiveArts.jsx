9537bdb6c4a8786dc39bf8fa0bf437d27ed761a9
EDA <james.panaho@gmail.com>
2020-01-31 10:30:33 +1300

    Right side almost done

--- client/components/GalleryFiveArts.jsx
+++ client/components/GalleryFiveArts.jsx
@@ -4,8 +4,14 @@ import { Link, Route } from 'react-router-dom'
 const GalleryFiveArts = (props) => {
   return (
     <div>
-    {console.log('Gal5Arts')}
-    <img src="" />
+      {console.log('Gal5Arts')}
+      <Link to="/">Hide 5x5 Arts</Link>
+      <div>
+        <img className="pic-sizer" src='5x5-1.png' />
+        <img className="pic-sizer" src='5x5-2.png' />
+        <img className="pic-sizer" src='5x5-3.png' />
+        <img className="pic-sizer" src='5x5-4.png' />
+      </div>
     </div>
 
   )
--- client/components/Right.jsx
+++ client/components/Right.jsx
@@ -4,15 +4,17 @@ import { Link, Route } from 'react-router-dom'
 import GalleryFive from './GalleryFive'
 import GalleryEight from './GalleryEight'
 import GalleryFiveArts from './GalleryFiveArts'
+import GalleryEightArts from './GalleryEightArts'
 
 const Right = (props) => {
   return (
     <>
     <h1>Art Gallery</h1>
     <div>
-      <Route path="/" component={ GalleryFive } />
+      <Route exact path="/" component={ GalleryFive } />
       <Route path="/gal5" component={ GalleryFiveArts } />
-      <Route path="/" component={ GalleryEight } />
+      <Route exact path="/" component={ GalleryEight } />
+      <Route path="/gal8" component={ GalleryEightArts } />
       {console.log('Main Right')}
     </div>
     </>
